5f4e4ee4cb285835dd89f34b544fe46447883d08
= <=>
2022-01-28 21:11:03 -0500

    added base json prices

--- server/pricing_functions.py
+++ server/pricing_functions.py
@@ -1,10 +1,12 @@
 import random
 import json
 
-pricing_info = ''
+pricing_info = None
 
 def calc_price(log):
     # reload pricing info from JSON
+    file = open('system_pricing.json')
+    pricing_info = json.load(file)
 
     # add pricings to base price
     price = 0
@@ -20,13 +22,16 @@ def calc_price(log):
     price += calc_computer_type(log)
     price += calc_computer_type(log)
     # floor price to typical price
+    price -= .01
 
-    #return final price
+    #close file and return final price
     if price > 0:
         return price
     return int(random.random()*940)-.01 + 60
 
 def calc_computer_type(log):
+    comp_type_prices = pricing_info['computer_type']
+    return comp_type_prices[log.computer_type]
     return 
 
 def calc_cpu_brand(log):
