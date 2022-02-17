c5e2be014d978daabc97f797aad871158a3a12fd
= <=>
2022-02-17 12:34:17 -0500

    added new file for universal javascript globals

---
+++ client/static/general_globals.js
@@ -0,0 +1,4 @@
+//new image for empty tab
+let no_thoughts_head_empty = document.createElement('img');
+no_thoughts_head_empty.setAttribute('src', 'static/img/nthe.png');
+no_thoughts_head_empty.setAttribute('class', 'imgNTHE');
