25aab2e6a831c7ed23e5fe745b92bd03d8b1ecbc
= <=>
2022-02-25 21:09:26 -0500

    added js file for consolelogs - has console data

---
+++ client/static/js/consolelog.js
@@ -0,0 +1,22 @@
+let consoleList = {
+    "Nintendo": {
+        "Consoles": ["Nintendo Entertainment System (NES)", "Super Nintendo Entertainment System (SNES)", "Nintendo 64 (N64)", "GameCube", "Wii", "WiiU", "Switch", "Switch OLED"], 
+        "Handheld": ["Game & Watch", "Game Boy", "Game Boy Color", "Game Boy Advance", "Game Boy Advance SP", "DS", "DS Lite", "DSi", "DSi XL", "3DS", "3DS XL", "2DS", "2DS XL", "New 3DS", "New 3DS XL", "New 2DS", "New 2DS XL"]
+    },
+    "Sony": {
+        "Consoles": ["Playstation", "PS One", "Playstation 2", "Playstation 2 Slim", "Playstation 3", "Playstation 3 Slim", "Playstation 3 Super Slim", "Playstation 4", "Playstation 4 Slim", "Playstation 4 Pro", "Playstation 5"],
+        "Handhelds": ["Playstation Portable (PSP)", "Playstation Portable (PSP) [2000]", "Playstation Portable (PSP) [3000]","Playstation Portable Go (PSP Go) [N1000]", "Playstation Portable Street (PSP Street) [E1000]", "Playstation Vita (PS Vita)", "Playstation Vita Slim (PS Vita Slim)"]
+    },
+    "Microsoft": {
+        "Consoles": ["XBOX", "XBOX 360", "XBOX 360 S", "XBOX 360 E", "XBOX One", "XBOX One S", "XBOX One X", "XBOX Series X", "XBOX Series S"],
+        "Handhelds": ["lol u thought"]
+    },
+    "Sega": {
+        "Consoles": ["Master System", "Genesis/Mega Drive", "CD", "32X", "Saturn", "Dreamcast"],
+        "Handhelds": ["Game Gear"]
+    },
+    "Atari": {
+        "Consoles": ["2600", "5200", "7800", "Jaguar"],
+        "Handhelds": ["Lynx"]
+    }
+}
