a6d7b2e4f944b2e4379e893122ea910b5b0f644e
= <=>
2022-02-26 21:09:25 -0500

    changed handheld to handhelds

--- client/static/js/consolelog.js
+++ client/static/js/consolelog.js
@@ -1,7 +1,7 @@
 let consoleList = {
     "Nintendo": {
         "Consoles": ["Nintendo Entertainment System (NES)", "Super Nintendo Entertainment System (SNES)", "Nintendo 64 (N64)", "GameCube", "Wii", "WiiU", "Switch", "Switch OLED"], 
-        "Handheld": ["Game & Watch", "Game Boy", "Game Boy Color", "Game Boy Advance", "Game Boy Advance SP", "DS", "DS Lite", "DSi", "DSi XL", "3DS", "3DS XL", "2DS", "2DS XL", "New 3DS", "New 3DS XL", "New 2DS", "New 2DS XL"]
+        "Handhelds": ["Game & Watch", "Game Boy", "Game Boy Color", "Game Boy Advance", "Game Boy Advance SP", "DS", "DS Lite", "DSi", "DSi XL", "3DS", "3DS XL", "2DS", "2DS XL", "New 3DS", "New 3DS XL", "New 2DS", "New 2DS XL"]
     },
     "Sony": {
         "Consoles": ["Playstation", "PS One", "Playstation 2", "Playstation 2 Slim", "Playstation 3", "Playstation 3 Slim", "Playstation 3 Super Slim", "Playstation 4", "Playstation 4 Slim", "Playstation 4 Pro", "Playstation 5"],
